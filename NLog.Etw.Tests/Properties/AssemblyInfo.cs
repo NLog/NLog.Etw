2ec57c5e602a57da295f89258c5a5ec0a410df0a
Julian Verdurmen <304NotModified@users.noreply.github.com>
2015-08-25 23:33:11 +0200

    merge with master

--- NLog.Etw.Tests/Properties/AssemblyInfo.cs
+++ NLog.Etw.Tests/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("NLog.Etw.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NLog.Etw.Tests")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6a7cd571-bee3-4629-a750-76dc77a5144f")]
+[assembly: Guid("242ce3ce-ca46-451a-86e3-e60386f18935")]
 
 // Version information for an assembly consists of the following four values:
 //
--- NLog.Etw/Properties/AssemblyInfo.cs
+++ NLog.Etw/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("NLog.Etw")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
+[assembly: AssemblyCopyright("Copyright © NLog")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
