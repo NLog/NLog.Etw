9c8f9d4d3a9fad1b68ddf6b17fd1c97039f3f6a8
Rolf Kristensen <sweaty1@hotmail.com>
2019-01-13 23:05:07 +0100

    EtwEventSourceTarget - EventSource with dynamic ProviderName

---
+++ NLog.Etw.Tests/EtwEventSourceTargetTests.cs
@@ -0,0 +1,178 @@
+﻿using Microsoft.Diagnostics.Tracing;
+using Microsoft.Diagnostics.Tracing.Parsers;
+using Microsoft.Diagnostics.Tracing.Session;
+using NLog.Config;
+using NLog.Layouts;
+using NLog.Targets;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using Xunit;
+
+namespace NLog.Etw.Tests
+{
+    public class EtwEventSourceTargetTests
+    {
+        private readonly EtwEventSourceTarget etwTarget;
+
+        public EtwEventSourceTargetTests()
+        {
+            // setup NLog configuration
+            var loggingConfiguration = new LoggingConfiguration();
+            this.etwTarget = new EtwEventSourceTarget() { ProviderName = nameof(EtwEventSourceTargetTests), Layout = Layout.FromString("${uppercase:${level}}|${logger}|${message}") };
+
+            loggingConfiguration.LoggingRules.Add(new LoggingRule("*", LogLevel.Debug, this.etwTarget));
+            loggingConfiguration.AddTarget("etw", this.etwTarget);
+            LogManager.Configuration = loggingConfiguration;
+        }
+
+        [Fact]
+        public void Writing_Message_To_Etw()
+        {
+            var resetEvent = new ManualResetEvent(false);
+            var collectedEvents = new List<NLogEtwEventData>(5);
+
+            var providerName = etwTarget.ProviderName.Render(LogEventInfo.CreateNullEvent());
+
+            using (var session = new TraceEventSession("SimpleMonitorSession"))
+            {
+                // Dynamic-Parser does not work with pure EventSource-objects
+                session.Source.Registered.All += delegate (TraceEvent data)
+                {
+                    if (data.Level == TraceEventLevel.Always)
+                        return;   // Not ours
+
+                    collectedEvents.Add(new NLogEtwEventData(data)
+                    {
+                        EventId = 0, // Raw EventSource gives "random" EventId
+                        LoggerName = (string)data.PayloadByName("LoggerName"),
+                        Message = (string)data.PayloadByName("Message"),
+                    });
+
+                    if (collectedEvents.Count == 5)
+                    {
+                        resetEvent.Set();
+                    }
+                };
+                session.Source.UnhandledEvents += delegate (TraceEvent data)
+                {
+                    if ((int)data.ID == 0xFFFE)
+                        return; // The EventSource manifest events show up as unhanded, filter them out.
+                };
+
+                var task = System.Threading.Tasks.Task.Run(() => session.Source.Process());
+
+                Thread.Sleep(1000);
+
+                session.EnableProvider(providerName);
+
+                Thread.Sleep(1000);
+
+                // send events to session
+                var logger = LogManager.GetLogger("A");
+                logger.Debug("test-debug");
+                logger.Info("test-info");
+                logger.Warn("test-warn");
+                logger.Error("test-error");
+                logger.Fatal("test-fatal");
+
+                try
+                {
+                    var eventSourceGuid = TraceEventProviders.GetEventSourceGuidFromName(providerName);
+                    session.DisableProvider(eventSourceGuid);
+                }
+                catch
+                {
+                    Thread.Sleep(1000);
+                    var eventSourceGuid = TraceEventProviders.GetEventSourceGuidFromName(providerName);
+                    session.DisableProvider(eventSourceGuid);
+                }
+
+                Thread.Sleep(1000);
+
+                logger.Fatal("don't log this one");
+                resetEvent.WaitOne(20000);
+            }
+
+            // assert collected events
+            var expectedEvents = new NLogEtwEventData[] {
+                new NLogEtwEventData { EventId = 0, ProviderName = providerName, TaskName = "Debug", Level = TraceEventLevel.Verbose, LoggerName = "A", Message = "DEBUG|A|test-debug" },
+                new NLogEtwEventData { EventId = 0, ProviderName = providerName, TaskName = "Info", Level = TraceEventLevel.Informational, LoggerName = "A", Message = "INFO|A|test-info" },
+                new NLogEtwEventData { EventId = 0, ProviderName = providerName, TaskName = "Warn", Level = TraceEventLevel.Warning, LoggerName = "A", Message = "WARN|A|test-warn" },
+                new NLogEtwEventData { EventId = 0, ProviderName = providerName, TaskName = "Error", Level = TraceEventLevel.Error, LoggerName = "A", Message = "ERROR|A|test-error" },
+                new NLogEtwEventData { EventId = 0, ProviderName = providerName, TaskName = "Fatal", Level = TraceEventLevel.Critical,  LoggerName = "A", Message = "FATAL|A|test-fatal" }
+            };
+            Assert.Equal(collectedEvents, expectedEvents);
+        }
+
+        [Fact]
+        public void Writing_Exception_To_Etw()
+        {
+            var resetEvent = new ManualResetEvent(false);
+            var collectedEvents = new List<NLogEtwEventData>(5);
+
+            var providerName = etwTarget.ProviderName.Render(LogEventInfo.CreateNullEvent());
+
+            using (var session = new TraceEventSession("SimpleMonitorSession"))
+            {
+                // Dynamic-Parser does not work with pure EventSource-objects
+                session.Source.Registered.All += delegate (TraceEvent data)
+                {
+                    if (data.Level == TraceEventLevel.Always)
+                        return;   // Not ours
+
+                    string exception = ((string)data.PayloadByName("Exception"));
+                    exception = exception?.Substring(0, exception.IndexOf(Environment.NewLine)) ?? string.Empty;
+
+                    collectedEvents.Add(new NLogEtwEventData(data)
+                    {
+                        EventId = 0, // Raw EventSource gives "random" EventId
+                        LoggerName = (string)data.PayloadByName("LoggerName"),
+                        Message = (string)data.PayloadByName("Message") + "|" + exception,
+                    });
+
+                    if (collectedEvents.Count == 1)
+                    {
+                        resetEvent.Set();
+                    }
+                };
+                session.Source.UnhandledEvents += delegate (TraceEvent data)
+                {
+                    if ((int)data.ID == 0xFFFE)
+                        return; // The EventSource manifest events show up as unhanded, filter them out.
+                };
+
+                var task = System.Threading.Tasks.Task.Run(() => session.Source.Process());
+
+                Thread.Sleep(1000);
+
+                session.EnableProvider(providerName);
+
+                Thread.Sleep(1000);
+
+                // send events to session
+                var logger = LogManager.GetLogger("A");
+                try
+                {
+                    if (!task.IsCanceled)
+                        throw new InvalidProgramException("Best exception in the world");
+                }
+                catch (Exception ex)
+                {
+                    logger.Fatal(ex, "test-fatal");
+                }
+
+                resetEvent.WaitOne(20000);
+            }
+
+            // assert collected events
+            var expectedEvents = new NLogEtwEventData[] {
+                new NLogEtwEventData { EventId = 0, ProviderName = providerName, TaskName = "Fatal", Level = TraceEventLevel.Critical,  LoggerName = "A", Message = "FATAL|A|test-fatal|System.InvalidProgramException: Best exception in the world" }
+            };
+            Assert.Equal(collectedEvents, expectedEvents);
+        }
+    }
+}
+
---
+++ NLog.Etw/EtwEventSourceTarget.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Concurrent;
+#if NET45
+using Microsoft.Diagnostics.Tracing;
+#else
+using System.Diagnostics.Tracing;
+#endif
+using NLog.Config;
+using NLog.Layouts;
+
+namespace NLog.Targets
+{
+    /// <summary>
+    /// NLog Target to ETW EventSource. Supporting dynamic EventSource name
+    /// </summary>
+    [Target("EtwEventSource")]
+    public class EtwEventSourceTarget : TargetWithLayout
+    {
+        private static readonly ConcurrentDictionary<string, EventSource> EventSources = new ConcurrentDictionary<string, EventSource>(System.StringComparer.OrdinalIgnoreCase);
+
+        /// <summary>
+        /// Name used for the EventSouce contructor
+        /// </summary>
+        [RequiredParameter]
+        public Layout ProviderName { get; set; }
+
+        /// <summary>
+        /// Context TaskName for EventData
+        /// </summary>
+        public Layout EventTaskName { get; set; } = "${level}";
+
+        private EventSource _eventSource;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EtwEventSourceTarget"/> class.
+        /// </summary>
+        public EtwEventSourceTarget()
+        {
+            Layout = "${message}";  // Timestamp + LogLevel is included in the ETW-EventData
+        }
+
+        /// <inheritdoc/>
+        protected override void InitializeTarget()
+        {
+            var providerName = (ProviderName?.Render(LogEventInfo.CreateNullEvent()) ?? string.Empty).Trim();
+            if (string.IsNullOrEmpty(providerName))
+            {
+                throw new NLogConfigurationException("EtwEventSourceTarget - ProviderName must be configured");
+            }
+
+            if (!EventSources.TryGetValue(providerName, out _eventSource))
+            {
+                _eventSource = new EventSource(providerName, EventSourceSettings.EtwSelfDescribingEventFormat);
+                if (_eventSource.ConstructionException != null)
+                {
+                    NLog.Common.InternalLogger.Error("EtwEventSourceTarget(Name={0}): EventSource({1}) constructor threw exception: {2}", Name, providerName, _eventSource.ConstructionException);
+                }
+                if (!EventSources.TryAdd(providerName, _eventSource))
+                {
+                    _eventSource = EventSources[providerName];
+                }
+            }
+
+            base.InitializeTarget();
+        }
+
+        /// <summary>
+        /// Write LogEvent to ETW.
+        /// </summary>
+        /// <param name="logEvent">event to be written.</param>
+        protected override void Write(LogEventInfo logEvent)
+        {
+            if (_eventSource?.IsEnabled() == true)
+            {
+                if (logEvent.Level == LogLevel.Debug || logEvent.Level == LogLevel.Trace)
+                {
+                    WriteEvent(logEvent, EventLevel.Verbose);
+                }
+                else if (logEvent.Level == LogLevel.Info)
+                {
+                    WriteEvent(logEvent, EventLevel.Informational);
+                }
+                else if (logEvent.Level == LogLevel.Warn)
+                {
+                    WriteEvent(logEvent, EventLevel.Warning);
+                }
+                else if (logEvent.Level == LogLevel.Error)
+                {
+                    WriteEvent(logEvent, EventLevel.Error);
+                }
+                else //if (logEvent.Level == LogLevel.Fatal)
+                {
+                    WriteEvent(logEvent, EventLevel.Critical);
+                }
+            }
+        }
+
+        private void WriteEvent(LogEventInfo logEvent, EventLevel level)
+        {
+            if (_eventSource.IsEnabled(level, EventKeywords.None))
+            {
+                var message = Layout.Render(logEvent);
+                var taskName = EventTaskName?.Render(logEvent) ?? string.Empty;
+
+                var sourceOptions = new EventSourceOptions();
+                sourceOptions.Level = level;
+                sourceOptions.Opcode = EventOpcode.Info;
+                if (logEvent.Exception != null)
+                {
+                    var eventData = new NLogEventDataException();
+                    eventData.LoggerName = logEvent.LoggerName;
+                    eventData.Message = message;
+                    eventData.Exception = logEvent.Exception.ToString();
+                    _eventSource.Write(taskName, ref sourceOptions, ref eventData);
+                }
+                else
+                {
+                    var eventData = new NLogEventData();
+                    eventData.LoggerName = logEvent.LoggerName;
+                    eventData.Message = message;
+                    _eventSource.Write(taskName, ref sourceOptions, ref eventData);
+                }
+            }
+        }
+
+        [EventData]
+        private struct NLogEventData
+        {
+            public string LoggerName { get; set; }
+            public string Message { get; set; }
+        }
+
+        [EventData]
+        private struct NLogEventDataException
+        {
+            public string LoggerName { get; set; }
+            public string Message { get; set; }
+            public string Exception { get; set; }
+        }
+    }
+}
