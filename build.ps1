ea786f1fcd262830ffb500dc916102906d32b956
Julian Verdurmen <5808377+304NotModified@users.noreply.github.com>
2022-08-16 22:10:09 +0200

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -10,7 +10,7 @@ if ($env:APPVEYOR_PULL_REQUEST_NUMBER) {
     $versionSuffix = "PR" + $env:APPVEYOR_PULL_REQUEST_NUMBER
 }
 
-msbuild /t:pack /p:Configuration=Release /p:IncludeSymbols=true /p:SymbolPackageFormat=snupkg /maxcpucount /p:PackageOutputPath=..\artifacts /p:VersionPrefix=$versionPrefix /p:VersionSuffix=$versionSuffix /p:FileVersion=$versionFile /p:ContinuousIntegrationBuild=true /p:EmbedUntrackedSources=true /verbosity:minimal
+msbuild /t:restore,pack /p:Configuration=Release /p:IncludeSymbols=true /p:SymbolPackageFormat=snupkg /maxcpucount /p:PackageOutputPath=..\artifacts /p:VersionPrefix=$versionPrefix /p:VersionSuffix=$versionSuffix /p:FileVersion=$versionFile /p:ContinuousIntegrationBuild=true /p:EmbedUntrackedSources=true /verbosity:minimal
 if (-Not $LastExitCode -eq 0) {
     exit $LastExitCode 
 }
