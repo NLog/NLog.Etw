f105a3ef34a3574624a74054b6e8b9f289814025
Julian Verdurmen <5808377+304NotModified@users.noreply.github.com>
2022-08-16 22:14:26 +0200

    NLog 5 and version 5-rc1 (#51)
    * NLog 5.0.2
    
    * Update appveyor.yml
    
    * AssemblyVersion
    
    * PackageLicenseExpression
    
    * PackageIcon
    
    * removal of PackageLicenseUrl
    
    * Update NLog.Etw.csproj
    
    * Update NLog.Etw.csproj
    
    * Create build.ps1
    
    * Update appveyor.yml
    
    * Update build.ps1
    
    * Update build.ps1
    
    * Update build.ps1

---
+++ build.ps1
@@ -0,0 +1,18 @@
+# restore and builds all projects as release.
+# creates NuGet package at \artifacts
+dotnet --version
+
+$versionPrefix = "5.0.0" # Also update version for minor versions in appveyor.yml 
+$versionSuffix = "rc1"
+$versionFile = $versionPrefix + "." + ${env:APPVEYOR_BUILD_NUMBER}
+if ($env:APPVEYOR_PULL_REQUEST_NUMBER) {
+    $versionPrefix = $versionFile
+    $versionSuffix = "PR" + $env:APPVEYOR_PULL_REQUEST_NUMBER
+}
+
+msbuild /t:restore,pack /p:Configuration=Release /p:IncludeSymbols=true /p:SymbolPackageFormat=snupkg /maxcpucount /p:PackageOutputPath=..\artifacts /p:VersionPrefix=$versionPrefix /p:VersionSuffix=$versionSuffix /p:FileVersion=$versionFile /p:ContinuousIntegrationBuild=true /p:EmbedUntrackedSources=true /verbosity:minimal
+if (-Not $LastExitCode -eq 0) {
+    exit $LastExitCode 
+}
+
+exit $LastExitCode
